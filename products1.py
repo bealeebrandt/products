3ed004484125348deeea2ff0dae537505eecf43f
bealeebrandt <70760839+bealeebrandt@users.noreply.github.com>
2020-11-11 10:39:21 +0100

    Add files via upload

--- products1.py
+++ products1.py
@@ -1,4 +1,12 @@
 products = []
+with open('products.txt', 'r') as f:
+    for line in f:
+        if 'Product, Price' in line:
+            continue
+        name, price = line.strip().split(',')
+        products.append([name, price])
+print(products)
+
 while True:
     name = input('Please insert the name of the product: ')
     if name == 'q':
@@ -12,4 +20,8 @@ print(products)
 
 for p in products:
     print(p[0], 'price is', p[1])
-    
+
+with open('products.txt', 'w') as f:
+    f.write('Product, Price\n')
+    for p in products:
+        f.write(p[0] + ',' + str(p[1]) + '\n')
